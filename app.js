da19b687796626b1700b4ccb9abe119945cb9a91
jjeongsu <jeongsu.dev@gmail.com>
2023-10-21 17:10:16 +0900

    [CICD] add deploy.yml

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const app = require('express')
+const express = require('express')
+const app = express()
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.status(200).send('<h1>hello</h1>')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('server is running..')
 })
